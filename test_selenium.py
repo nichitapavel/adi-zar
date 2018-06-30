c98fa4c1c9fe916c0e0f7061008a53c6ee5bda4c
Pavel Nichita <nichitapavel@live.com>
2018-06-30 15:37:56 +0100

    -Use pytest setup & teardown

--- test_selenium.py
+++ test_selenium.py
@@ -1,35 +1,43 @@
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.webdriver import WebDriver
 from text_generator.text_generator import TextGenerator
+from sample_page import SamplePage
 
-FORM_COMMENT_ID = 'comment'
-FORM_NAME_ID = 'author'
-FORM_EMAIL_ID = 'email'
-FORM_ID = 'commentform'
 
-COMMENT_BODY = 'comment-body'
+# HTML ID's
+COMMENT_BODY_ID = 'comment-body'
+ERROR_MESSAGE_ID = 'error-page'
 
-LINK_SAMPLE_PAGE = 'Sample Page'
+# HTML Text Link
+SAMPLE_PAGE = 'Sample Page'
 
-USERNAME = 'Pavel'
+# HTML messages
+ERROR_MESSAGE = 'ERROR: please enter a valid email address.'
+
+# HMTL Form data
+NAME = 'Pavel'
 WRONG_EMAIL = 'awesome at ten dot net'
 CORRECT_EMAIL = 'awesome@ten.net'
 
+# Root directory to save data in case of screenshots
 DIR = 'photos'
 
 
 class TestSelenium:
 
     driver = None
+    sample_page = None
 
-    def setup(self):
         self.driver = WebDriver(command_executor='http://192.168.0.165:4444/wd/hub',
                                 desired_capabilities=DesiredCapabilities.CHROME.copy())
         self.driver.get('http://store.demoqa.com')
+        self.navigate_to_page(SAMPLE_PAGE)
+        self.sample_page = SamplePage(self.driver)
 
-    def teardown(self):
+        self.driver.close()
         self.driver.quit()
         self.driver = None
+        self.sample_page = None
 
     def navigate_to_page(self, link_text):
         self.driver.find_element_by_link_text(link_text).click()
@@ -42,79 +50,70 @@ class TestSelenium:
         text.generate_dictionary(size)
         return ' '.join(map(str, text.get_dictionary()))
 
-    def test_wrong_email_redirects_error_page(self):
-        # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
-
-        # when
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(self.generate_string(100))
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(WRONG_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/11-KO-wrong-email.png')
-        comment_form.submit()
-
-        # then
-        self.driver.save_screenshot(f'{DIR}/12-KO-error-page.png')
-        assert self.driver.title == 'Comment Submission Failure'
-
-    def test_correct_email_adds_comment(self):
-        # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
-
+    def test_wrong_email_redirects_error_page(self, request):
+        """
+        Writes a comment with wrong email, saves a screenshot.
+        Checks that error messages appears.
+        :param request: pytest fixture with built in methods
+        :return:
+        """
         # when
         comment = self.generate_string(100)
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(comment)
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(CORRECT_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/21-OK-good-email.png')
-        comment_form.submit()
+        self.sample_page.write_comment(comment, NAME, WRONG_EMAIL)
+        self.driver.save_screenshot(f'{DIR}/{request.node.name}.png')
 
         # then
-        self.driver.save_screenshot(f'{DIR}/22-OK-comment-post.png')
-        # return url: http://store.demoqa.com/sample-page/#comment-620
-        new_comment_id = self.driver.current_url.split('#')[-1]
-        comment_found = self.driver.find_element_by_id(new_comment_id)
-        assert comment_found is not None
-        assert comment_found.find_element_by_class_name(COMMENT_BODY).text == comment
+        error = self.driver.find_element_by_id(ERROR_MESSAGE_ID)
+        assert ERROR_MESSAGE in error.text
 
-    def test_of_the_gods(self):
-        # Enter a comment with a wrong email & Check Error is displayed
-        # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
-
+    def test_correct_email_adds_comment(self, request):
+        """
+        Writes a comment with correct email, saves a screenshot.
+        Checks that comment was inserted and it has the correct value.
+        :param request: pytest fixture with built in methods
+        :return:
+        """
         # when
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(self.generate_string(100))
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(WRONG_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/31-GOD-KO-wrong-email.png')
-        comment_form.submit()
+        comment = self.generate_string(100)
+        self.sample_page.write_comment(comment, NAME, CORRECT_EMAIL)
 
         # then
-        self.driver.save_screenshot(f'{DIR}/32-GOD-KO-error-page.png')
-        assert self.driver.title == 'Comment Submission Failure'
+        # TODO self.driver.current_url works, but is a bit out of context, maybe it should be returned by .write_comment
+        comment_found = self.sample_page.get_comment(self.driver.current_url)
+        self.driver.save_screenshot(f'{DIR}/{request.node.name}.png')
 
-        # Navigate back
+        assert comment_found is not None
+        assert comment == comment_found.find_element_by_class_name(COMMENT_BODY_ID).text
+
+    def test_of_the_gods(self, request):
+        """
+        Does everything, writes a comment with wrong email, checks that error messages appears, goes back,
+        writes a new comment with correct email, saves a screenshot, checks that comment was inserted and it
+        has the correct value.
+        :param request: pytest fixture with built in methods
+        :return:
+        """
+        # Enter a comment with a wrong email & Check Error is displayed
+        # when
+        self.sample_page.write_comment(self.generate_string(100), NAME, WRONG_EMAIL)
+
+        # then
+        error = self.driver.find_element_by_id(ERROR_MESSAGE_ID)
+        assert ERROR_MESSAGE in error.text
+
+        # Navigate to Sample Page
         self.driver.back()
 
         # Enter a comment with a correct email & Check Comment is Received
-        # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
-
         # when
+        # Chrome keeps values previously typed
+        self.sample_page.clear_fields()
         comment = self.generate_string(100)
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(comment)
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(CORRECT_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/33-GOD-OK-good-email-2.png')
-        comment_form.submit()
+        self.sample_page.write_comment(comment, NAME, CORRECT_EMAIL)
 
-        # then
-        self.driver.save_screenshot(f'{DIR}/34-GOD-OK-comment-post.png')
-        new_comment_id = self.driver.current_url.split('#')[-1]
-        comment_found = self.driver.find_element_by_id(new_comment_id)
+        # TODO self.driver.current_url works, but is a bit out of context, maybe it should be returned by .write_comment
+        comment_found = self.sample_page.get_comment(self.driver.current_url)
+        self.driver.save_screenshot(f'{DIR}/{request.node.name}.png')
+
         assert comment_found is not None
-        assert comment_found.find_element_by_class_name(COMMENT_BODY).text == comment
+        assert comment_found.find_element_by_class_name(COMMENT_BODY_ID).text == comment
