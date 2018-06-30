fa217deb8afd5900ad59130cda52cdd1f1e3bd26
Pavel Nichita <nichitapavel@live.com>
2018-06-30 16:11:48 +0100

    -Use methods form SamplePage class

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.webdriver import WebDriver
 from text_generator.text_generator import TextGenerator
+from sample_page import SamplePage
 
 # HTML ID's
 COMMENT_BODY_ID = 'comment-body'
@@ -20,15 +21,20 @@ DIR = 'photos'
 class TestSelenium:
 
     driver = None
+    sample_page = None
 
     def setup_method(self):
         self.driver = WebDriver(command_executor='http://192.168.0.165:4444/wd/hub',
                                 desired_capabilities=DesiredCapabilities.CHROME.copy())
         self.driver.get('http://store.demoqa.com')
+        self.navigate_to_page(SAMPLE_PAGE)
+        self.sample_page = SamplePage(self.driver)
 
     def teardown_method(self):
+        self.driver.close()
         self.driver.quit()
         self.driver = None
+        self.sample_page = None
 
     def navigate_to_page(self, link_text):
         self.driver.find_element_by_link_text(link_text).click()
@@ -43,35 +49,24 @@ class TestSelenium:
 
     def test_wrong_email_redirects_error_page(self):
         # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
+        self.navigate_to_page(SAMPLE_PAGE)
 
         # when
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(self.generate_string(100))
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(WRONG_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/11-KO-wrong-email.png')
-        comment_form.submit()
+        comment = self.generate_string(100)
+        self.sample_page.write_comment(comment, NAME, WRONG_EMAIL)
 
         # then
-        self.driver.save_screenshot(f'{DIR}/12-KO-error-page.png')
         assert self.driver.title == 'Comment Submission Failure'
 
     def test_correct_email_adds_comment(self):
         # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
+        self.navigate_to_page(SAMPLE_PAGE)
 
         # when
         comment = self.generate_string(100)
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(comment)
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(CORRECT_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/21-OK-good-email.png')
-        comment_form.submit()
+        self.sample_page.write_comment(comment, NAME, CORRECT_EMAIL)
 
         # then
-        self.driver.save_screenshot(f'{DIR}/22-OK-comment-post.png')
         # return url: http://store.demoqa.com/sample-page/#comment-620
         new_comment_id = self.driver.current_url.split('#')[-1]
         comment_found = self.driver.find_element_by_id(new_comment_id)
@@ -81,15 +76,10 @@ class TestSelenium:
     def test_of_the_gods(self):
         # Enter a comment with a wrong email & Check Error is displayed
         # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
+        self.navigate_to_page(SAMPLE_PAGE)
 
         # when
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(self.generate_string(100))
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(WRONG_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/31-GOD-KO-wrong-email.png')
-        comment_form.submit()
+        self.sample_page.write_comment(self.generate_string(100), NAME, WRONG_EMAIL)
 
         # then
         self.driver.save_screenshot(f'{DIR}/32-GOD-KO-error-page.png')
@@ -100,16 +90,9 @@ class TestSelenium:
 
         # Enter a comment with a correct email & Check Comment is Received
         # given
-        self.navigate_to_page(LINK_SAMPLE_PAGE)
-
-        # when
+        self.sample_page.clear_fields()
         comment = self.generate_string(100)
-        comment_form = self.driver.find_element_by_id(FORM_ID)
-        comment_form.find_element_by_id(FORM_COMMENT_ID).send_keys(comment)
-        comment_form.find_element_by_id(FORM_NAME_ID).send_keys(USERNAME)
-        comment_form.find_element_by_id(FORM_EMAIL_ID).send_keys(CORRECT_EMAIL)
-        self.driver.save_screenshot(f'{DIR}/33-GOD-OK-good-email-2.png')
-        comment_form.submit()
+        self.sample_page.write_comment(comment, NAME, CORRECT_EMAIL)
 
         # then
         self.driver.save_screenshot(f'{DIR}/34-GOD-OK-comment-post.png')
